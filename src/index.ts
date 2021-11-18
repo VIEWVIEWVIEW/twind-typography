a7651d8f7e717e006306b1694593ebc60bd19937
Pathetic Geek <geekpathetic@gmail.com>
2021-11-18 20:04:41 +0530

    ✨ Add basic implementation with dark theme

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,66 @@
-const main = () => {
-  console.log('I AM a seperate package')
+import type { CSSRules, Plugin, ThemeSection } from 'twind'
+
+import { styles } from './styles'
+
+export interface Options {
+  className?: string
 }
 
-main()
+declare module 'twind' {
+  interface Theme {
+    typography?: ThemeSection<{ css?: CSSRules }>
+  }
+}
 
-export default main
+const typography = ({ className = 'prose' }: Options = {}): Record<
+  string,
+  Plugin
+> => ({
+  lead: (_, { tag }) => tag('lead'),
+
+  [className]: (parameters, context) => {
+    const config = styles(context)
+    const size = parameters[0] || 'DEFAULT'
+    const { css } = config[size] || {}
+    const overwriteCss = context.theme('typography')[size] || {}
+
+    const rules: CSSRules = {}
+
+    css.forEach((css) =>
+      Object.entries(css).forEach(([property, value]) => {
+        const key =
+          value != null && typeof value === 'object' && !Array.isArray(value)
+            ? `& ${property}`
+            : property
+
+        const existingRules = rules[key]
+        if (typeof existingRules === 'object' && typeof value === 'object') {
+          rules[key] = { ...existingRules, ...value }
+        } else {
+          rules[key] = value
+        }
+      })
+    )
+
+    if (overwriteCss.css) {
+      console.log('overwriteCss', overwriteCss)
+      Object.entries(overwriteCss.css).forEach(([property, value]) => {
+        const key =
+          value != null && typeof value === 'object' && !Array.isArray(value)
+            ? `& ${property}`
+            : property
+
+        const existingRules = rules[key]
+        if (typeof existingRules === 'object' && typeof value === 'object') {
+          rules[key] = { ...existingRules, ...value }
+        } else {
+          rules[key] = value
+        }
+      })
+    }
+
+    return rules
+  }
+})
+
+export default typography
